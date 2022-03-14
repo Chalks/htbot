ab7ab45a5070f867d9b08c2385ae6af479f7dd4d
Jonathan Walters <skychalk@gmail.com>
2022-03-14 18:55:15 -0400

    minor bugs, still not quite right but getting there

--- bot/functions/hello.js
+++ bot/functions/hello.js
@@ -1,6 +1,8 @@
 const nacl = require('tweetnacl');
-
 const {PUBLIC_KEY} = require('./config.json');
+const CARD_DB = require('./cards.json');
+
+const CARDS = CARD_DB[CARD_DB.latestVersion];
 
 const verify = async ({signature, timestamp, body}) => {
     try {
@@ -16,28 +18,56 @@ const verify = async ({signature, timestamp, body}) => {
     }
 };
 
+const reject = (reason) => ({
+    statusCode: 401,
+    body: reason,
+});
+
 const pong = () => ({
     statusCode: 200,
     headers: {'Content-Type': 'application/json'},
     body: JSON.stringify({type: 1}),
 });
 
+const handleCommand = ({data}) => {
+    const {name, options} = data;
+    if (name !== 'card') return reject('Invalid command name');
+
+    const option = options[0]; // TODO
+    const cardname = option.name;
+    const cardDescription = 'lel';
+
+    return {
+        statusCode: 200,
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            type: 4,
+            data: {
+                tts: false,
+                content: `${cardname} - ${cardDescription}`,
+                embeds: [],
+                allowed_mentions: {parse: []},
+            },
+        }),
+    };
+};
+
 exports.handler = async ({headers, body}) => {
     const {'x-signature-ed25519': signature, 'x-signature-timestamp': timestamp} = headers;
     const verified = await verify({signature, timestamp, body});
 
     if (!verified) {
-        return {
-            statusCode: 401,
-            body: 'invalid request signature',
-        };
+        return reject('Invalid request signature');
     }
 
-    const {type} = JSON.parse(body);
+    const jsonBody = JSON.parse(body);
+    const {type} = jsonBody;
 
     switch (type) {
         case 1:
             return pong();
+        case 2:
+            return handleCommand(jsonBody);
         default:
             return {
                 statusCode: 200,
--- bot/setDevCommands.js
+++ bot/setDevCommands.js
@@ -7,6 +7,7 @@ const cardDb = require('./functions/cards.json');
 const url = `https://discord.com/api/v8/applications/${APPLICATION_ID}/guilds/${DEV_GUILD_ID}/commands`;
 
 const cards = cardDb[cardDb.latestVersion];
+const choices = cards.map(({name, value}) => ({name, value}));
 
 const setCommands = async () => {
     axios.post(url, {
@@ -18,7 +19,7 @@ const setCommands = async () => {
             description: 'Card to display',
             type: 3,
             required: true,
-            choices: cards,
+            choices,
         }],
     }, {
         headers: {
